30c9b00a8072c4c9e01f387caf62c20002def253
Pashupati Sah <psah@pmsquare.com>
2025-01-30 13:30:34 +0545

    created a simple test button component

---
+++ components/TestButton.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <button>Modular button</button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'TestButton'
+}
+</script>
---
+++ index.js
@@ -0,0 +1,10 @@
+// Export all components
+export { default as TestButton } from './components/TestButton.vue';
+// export { default as Com2 } from './components/com2.vue';
+// export { default as Com3 } from './components/com3.vue';
+
+// // Export store module
+// export { default as page2Store } from './store/page2.js';
+
+// // Export a plugin (if you have one)
+// export { default as cityCrudPlugin } from './plugins/city-crud-store.js';
